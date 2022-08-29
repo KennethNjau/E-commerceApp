219522945e7e701cd6fa7ebd995976713036fb13
KennethNjau <kethnjon@gmail.com>
2022-08-29 13:08:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/e_commerceapp/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.e_commerceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+//A view group is an invinsible container of other views
+//(child views) and other viewGroups
+
+// view- is a basic building block of UI (user interface)in android
+
+//explicit intent- transitioning is done from one activity to another on the same app.
+//implicit intent- one application to another application.
